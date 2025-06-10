7adf43c3d7a1c286575836a95215fec334630895
Nyan Linn Hein <nnyanlinhein@gmail.com>
2025-06-10 16:15:49 +0630

    make sign up form and login form design

---
+++ src/components/Signup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="signup">
+    <h2>Sign Up</h2>
+    <form @submit.prevent="signUp">
+      <input type="text" placeholder="display name" v-model="displayName">
+      <input type="email" placeholder="email" v-model="email">
+      <input type="password" placeholder="password" v-model="password">
+      <button>Sign up</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup(){
+    let displayName = ref("")
+    let email = ref("")
+    let password = ref("")
+    let signUp =()=>{
+      console.log(displayName.value,email.value,password.value );
+    }
+    return {displayName,email,password,signUp}
+  }
+}
+</script>
+
+<style>
+
+</style>
