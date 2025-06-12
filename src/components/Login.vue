507ab03ee3470d754a332695dacfb1094ce056eb
Nyan Linn Hein <nnyanlinhein@gmail.com>
2025-06-12 13:57:51 +0630

    built login system but not push to any route yet

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,22 +4,30 @@
     <form @submit.prevent="login">
       <input type="email" placeholder="email" v-model="email">
       <input type="password" placeholder="password" v-model="password">
+      <div v-if="error" class="error">
+        {{ error }}
+      </div>
       <button>Login</button>
     </form>
   </div>
 </template>
 
 <script>
+import useLogin from '@/composable/useLogin';
 import { ref } from 'vue';
 
 export default {
   setup(){
     let email = ref("")
     let password = ref("")
-    let login =()=>{
-      console.log(email.value,password.value );
+    let {error,loginAccount} = useLogin()
+    let login =async()=>{
+      let res = await loginAccount(email.value,password.value)
+      if(res){
+        console.log(res.user);
+      }
     }
-    return {email,password,login}
+    return {email,password,login,error}
   }
 }
 </script>
